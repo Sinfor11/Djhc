aa3cd73253d3abc67ef244ebc363dcc7707a6a8b
DanielCarrasco <72710409+Sinfor11@users.noreply.github.com>
2021-10-12 19:19:05 -0500

    Update app11.js

--- Proyecto-JavaScript-main/practica-arrays/app11.js
+++ Proyecto-JavaScript-main/practica-arrays/app11.js
@@ -18,7 +18,7 @@ let textos = ( text ) => {
 
     }
     /*
-    for( let i = 0 ; i < newTexto.length ; i++ ){
+    for( let i = 1 ; i < newTexto.length ; i++ ){
         textoUnicode.push(newTexto[i].charCodeAt());
     }
     */
